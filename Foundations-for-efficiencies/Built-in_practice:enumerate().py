2689cab5c784ed61fcd9bd92062c7f387539d38a
RKiddle <RKiddle@users.noreply.github.com>
2019-12-11 21:33:52 +0700

    Create Built-in_practice:enumerate().py

---
+++ Foundations-for-efficiencies/Built-in_practice:enumerate().py
@@ -0,0 +1,14 @@
+# Rewrite the for loop to use enumerate
+indexed_names = []
+for i,range in enumerate(names):
+    index_name = (i,names[i])
+    indexed_names.append(index_name) 
+print(indexed_names)
+
+# Rewrite the above for loop using list comprehension
+indexed_names_comp = [(i, name) for i,name in enumerate(names)]
+print(indexed_names_comp)
+
+# Unpack an enumerate object with a starting index of one
+indexed_names_unpack = [*enumerate(names, start=1)]
+print(indexed_names_unpack)
